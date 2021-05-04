6ec06d8028b90738c9d45a13aa8785c2d5045750
Bram Renting <bramrenting@gmail.com>
2021-05-04 20:37:57 +0200

    FIX: change mount to absolute path

--- run.ps1
+++ run.ps1
@@ -1,7 +1,9 @@
 docker run `
--v ${PWD}/settings.yaml:app/settings.yaml `
+-v ${PWD}/settings.yaml:/app/settings.yaml `
 --mount type=bind,source=${PWD}/profiles,target=/app/profiles `
 --mount type=bind,source=${PWD}/parties,target=/app/parties `
 --mount type=bind,source=${PWD}/results,target=/app/results `
 --mount type=bind,source=${PWD}/tmp,target=/app/tmp `
 brenting/anl_2021:1.0.0
+
+pause
